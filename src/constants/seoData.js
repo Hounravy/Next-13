4a7291cb7cc5ce318ad9b36a0d904e94259c142c
RAVY <hounravy605@gmail.com>
2023-10-25 14:08:38 +0700

    update

--- src/constants/seoData.js
+++ src/constants/seoData.js
@@ -12,9 +12,9 @@ export const faqDefault = [
 export const seoDefualt = {
   linkTo: '/',
   seo: {
-    title: '買馬易 - 專業賭馬平台',
+    title: 'Next-13',
     description:
-      '買馬易是一個專業的買馬平台，讓你可以輕鬆地在線上賭馬，享受刺激的賽馬體驗。買馬易讓您觀看賽事、等待成績、享受最高賠率以最佳優惠活動。快來加入買馬易，享受最佳的賭馬服務吧！',
+      'Sample Description here',
     keywords: '',
     images: [],
     faq: faqDefault,
